9fdc99682334f7d3d11e0adc866c44573ab24b53
Rafeeahmed09 <scalablegrow@gmail.com>
2026-05-22 17:58:42 +0530

    new code

---
+++ hospital-management-system/src/main/java/NewProject/example/hospital/management/system/Entity/Appointment.java
@@ -0,0 +1,39 @@
+package NewProject.example.hospital.management.system.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.Builder;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@ToString
+@Getter
+@Builder
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentDate;
+
+    @Column(length = 500)
+    private String Description;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id") // Owing side , patient is required and not nullable
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Doctor doctor;
+}
